3a2b4831a7bfdc5f46003c642ac016e0aa61d777
Nicholas Funnell <nick@nick.gs>
2022-12-06 11:44:29 -0800

    day 5 part 1 done

--- 05.js
+++ 05.js
@@ -3,8 +3,13 @@ exports.__esModule = true;
 exports.exec = void 0;
 var fs = require("fs");
 function input() {
-    return fs.readFileSync("input-05-ex.txt", "utf-8").split("\n");
+    return fs.readFileSync("input-05.txt", "utf-8").split("\n");
 }
+/**
+ * returns -1 when done parsing the diagram
+ * returns 0 when empty input
+ * returns positive number to indicate that "ch" belongs to that stack number
+ */
 function parse(ch, i) {
     var ignore = "[] ";
     var numeric = "0123456789";
@@ -15,23 +20,82 @@ function parse(ch, i) {
         return -1;
     }
     var pos = Math.floor(i / 4);
-    console.log(ch, i, 1 + pos);
+    // console.log(ch, i, 1 + pos);
     return 1 + pos;
+}
+function buildStacks(stacks, line) {
+    for (var j = 0; j < line.length; j++) {
+        var a = parse(line[j], j);
+        if (a == -1) {
+            // console.log("done with it");
+            return false;
+        }
+        if (a > 0) {
+            // console.log("add", line[j], "to stack", a);
+            if (!stacks.hasOwnProperty(a)) {
+                stacks[a] = [];
+            }
+            stacks[a].unshift(line[j]);
+        }
+    }
+    return true;
+}
+function moveItems(command, stacks) {
+    console.log("COMMAND", command);
+    var parts = command.split(" ");
+    if (parts.length < 5) {
+        console.log("(skip)");
+        return;
+    }
+    var amount = parseInt(parts[1]);
+    var from = parseInt(parts[3]);
+    var to = parseInt(parts[5]);
+    for (var i = 0; i < amount; i++) {
+        var x = stacks[from].shift();
+        stacks[to].unshift(x);
+        console.log("Move", x, "From", from, "To", to);
+    }
+    printStacks(stacks);
+}
+function reverseStacks(stacks) {
+    for (var k in stacks) {
+        stacks[k].reverse();
+    }
 }
 function exec() {
     var lines = input();
+    var stacks = {};
+    var building = true;
     for (var i = 0; i < lines.length; i++) {
         var line = lines[i];
-        for (var j = 0; j < line.length; j++) {
-            var a = parse(line[j], j);
-            if (a == -1) {
-                console.log("done with it");
-                return;
-            }
-            if (a > 0) {
-                console.log("add", line[j], "to stack", a);
+        if (building) {
+            building = buildStacks(stacks, line);
+            if (!building) {
+                reverseStacks(stacks);
+                printStacks(stacks);
             }
         }
+        else {
+            moveItems(line, stacks);
+        }
     }
+    console.log("---", "Final State", "---");
+    printStacks(stacks);
 }
 exports.exec = exec;
+function printStacks(stacks) {
+    var max = -1;
+    for (var k in stacks) {
+        if (max < stacks[k].length) {
+            max = stacks[k].length;
+        }
+    }
+    for (var k in stacks) {
+        var buff = "";
+        var spaces = max - stacks[k].length;
+        for (var z = 0; z < spaces; z++) {
+            buff = buff + " ";
+        }
+        console.log("Stack", k, buff + stacks[k].join(""));
+    }
+}
--- 05.ts
+++ 05.ts
@@ -1,9 +1,14 @@
 const fs = require("fs");
 
 function input(): string[] {
-  return fs.readFileSync("input-05-ex.txt", "utf-8").split("\n");
+  return fs.readFileSync("input-05.txt", "utf-8").split("\n");
 }
 
+/**
+ * returns -1 when done parsing the diagram
+ * returns 0 when empty input
+ * returns positive number to indicate that "ch" belongs to that stack number
+ */
 function parse(ch: string, i: number): number {
   const ignore = "[] ";
   const numeric = "0123456789";
@@ -14,24 +19,86 @@ function parse(ch: string, i: number): number {
     return -1;
   }
   const pos = Math.floor(i / 4);
-  console.log(ch, i, 1 + pos);
+  // console.log(ch, i, 1 + pos);
   return 1 + pos;
+}
+
+function buildStacks(stacks, line) {
+  for (var j = 0; j < line.length; j++) {
+    const a = parse(line[j], j);
+    if (a == -1) {
+      // console.log("done with it");
+      return false;
+    }
+    if (a > 0) {
+      // console.log("add", line[j], "to stack", a);
+      if (!stacks.hasOwnProperty(a)) {
+        stacks[a] = [];
+      }
+      stacks[a].unshift(line[j]);
+    }
+  }
+  return true;
+}
+
+function moveItems(command, stacks) {
+  console.log("COMMAND", command);
+  const parts = command.split(" ");
+  if (parts.length < 5) {
+    console.log("(skip)");
+    return;
+  }
+  const amount = parseInt(parts[1]);
+  const from = parseInt(parts[3]);
+  const to = parseInt(parts[5]);
+  for (var i = 0; i < amount; i++) {
+    const x = stacks[from].shift();
+    stacks[to].unshift(x);
+    console.log("Move", x, "From", from, "To", to);
+  }
+  printStacks(stacks);
+}
+
+function reverseStacks(stacks) {
+  for (var k in stacks) {
+    stacks[k].reverse();
+  }
 }
 
 function exec() {
   const lines = input();
+  const stacks = {};
+  var building = true;
   for (var i = 0; i < lines.length; i++) {
     const line = lines[i];
-    for (var j = 0; j < line.length; j++) {
-      const a = parse(line[j], j);
-      if (a == -1) {
-        console.log("done with it");
-        return;
+    if (building) {
+      building = buildStacks(stacks, line);
+      if (!building) {
+        reverseStacks(stacks);
+        printStacks(stacks);
       }
-      if (a > 0) {
-        console.log("add", line[j], "to stack", a);
-      }
+    } else {
+      moveItems(line, stacks);
     }
+  }
+  console.log("---", "Final State", "---");
+  printStacks(stacks);
+}
+
+function printStacks(stacks) {
+  var max = -1;
+  for (var k in stacks) {
+    if (max < stacks[k].length) {
+      max = stacks[k].length;
+    }
+  }
+  for (var k in stacks) {
+    var buff = "";
+    const spaces = max - stacks[k].length;
+    for (var z = 0; z < spaces; z++) {
+      buff = buff + " ";
+    }
+    console.log("Stack", k, buff + stacks[k].join(""));
   }
 }
 
