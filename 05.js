a923b95a8abfb8b33d42fa3140b81cae92886796
Nicholas Funnell <nick@nick.gs>
2022-12-05 17:40:37 -0800

    can parse the stack diagram of day 5 beginning

--- 05.js
+++ 05.js
@@ -2,7 +2,36 @@
 exports.__esModule = true;
 exports.exec = void 0;
 var fs = require("fs");
+function input() {
+    return fs.readFileSync("input-05-ex.txt", "utf-8").split("\n");
+}
+function parse(ch, i) {
+    var ignore = "[] ";
+    var numeric = "0123456789";
+    if (ignore.indexOf(ch) >= 0) {
+        return 0;
+    }
+    if (numeric.indexOf(ch) >= 0) {
+        return -1;
+    }
+    var pos = Math.floor(i / 4);
+    console.log(ch, i, 1 + pos);
+    return 1 + pos;
+}
 function exec() {
-    console.log("day 5!");
+    var lines = input();
+    for (var i = 0; i < lines.length; i++) {
+        var line = lines[i];
+        for (var j = 0; j < line.length; j++) {
+            var a = parse(line[j], j);
+            if (a == -1) {
+                console.log("done with it");
+                return;
+            }
+            if (a > 0) {
+                console.log("add", line[j], "to stack", a);
+            }
+        }
+    }
 }
 exports.exec = exec;
--- 05.ts
+++ 05.ts
@@ -1,7 +1,38 @@
 const fs = require("fs");
 
+function input(): string[] {
+  return fs.readFileSync("input-05-ex.txt", "utf-8").split("\n");
+}
+
+function parse(ch: string, i: number): number {
+  const ignore = "[] ";
+  const numeric = "0123456789";
+  if (ignore.indexOf(ch) >= 0) {
+    return 0;
+  }
+  if (numeric.indexOf(ch) >= 0) {
+    return -1;
+  }
+  const pos = Math.floor(i / 4);
+  console.log(ch, i, 1 + pos);
+  return 1 + pos;
+}
+
 function exec() {
-  console.log("day 5!");
+  const lines = input();
+  for (var i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    for (var j = 0; j < line.length; j++) {
+      const a = parse(line[j], j);
+      if (a == -1) {
+        console.log("done with it");
+        return;
+      }
+      if (a > 0) {
+        console.log("add", line[j], "to stack", a);
+      }
+    }
+  }
 }
 
 export { exec };
