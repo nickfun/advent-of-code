f0738b52bf1233ec7afeeab860b585260fb72ed1
Nicholas Funnell <nick@nick.gs>
2022-12-09 16:18:36 -0800

    day 8 part 1 done

--- 2022/08.js
+++ 2022/08.js
@@ -2,10 +2,11 @@
 exports.__esModule = true;
 exports.exec = void 0;
 var fs = require("fs");
+var _ = require("lodash");
 function input() {
     var data = [[]];
     var raw = fs
-        .readFileSync("2022/input-08-ex.txt", "utf-8")
+        .readFileSync("2022/input-08.txt", "utf-8")
         .split("\n");
     for (var i = 0; i < raw.length; i++) {
         var line = raw[i];
@@ -18,8 +19,100 @@ function input() {
     }
     return data;
 }
+var Treemap = /** @class */ (function () {
+    function Treemap(rawmap) {
+        this.map = [[]];
+        this.width = 0;
+        this.height = 0;
+        this.map = rawmap;
+        this.width = rawmap[0].length;
+        this.height = rawmap.length;
+    }
+    Treemap.prototype.isVisible = function (x, y) {
+        var treeHeight = this.map[x][y];
+        var buff = [];
+        var bag = {
+            left: false,
+            right: false,
+            top: false,
+            bottom: false
+        };
+        // !! Left/Right compare
+        var row = this.map[x];
+        console.log("ROW:", row);
+        for (var i = 0; i <= row.length; i++) {
+            if (i < y) {
+                // left side
+                buff.push(this.map[x][i]);
+            }
+            if (i == y) {
+                // compare what is to left of us
+                bag.left = this.areAllBelow(buff, treeHeight, "left");
+                buff = [];
+            }
+            if (i > y && i != row.length) {
+                // build right side
+                buff.push(this.map[x][i]);
+            }
+            if (i == row.length) {
+                // compare what is right of us
+                bag.right = this.areAllBelow(buff, treeHeight, "right");
+            }
+        }
+        buff = [];
+        var column = [];
+        for (var i = 0; i < this.height; i++) {
+            column.push(this.map[i][y]);
+        }
+        console.log("COLUMN:", column);
+        for (var i = 0; i <= column.length; i++) {
+            if (i < x) {
+                // top
+                buff.push(column[i]);
+            }
+            if (i == x) {
+                // compare top
+                bag.top = this.areAllBelow(buff, treeHeight, "top");
+                buff = [];
+            }
+            if (i > x && i != column.length) {
+                buff.push(column[i]);
+            }
+            if (i == column.length) {
+                bag.bottom = this.areAllBelow(buff, treeHeight, "bottom");
+                buff = [];
+            }
+        }
+        var isVisible = bag.left || bag.right || bag.top || bag.bottom;
+        console.log("POS", x, y, "Value", treeHeight, "Visible?", isVisible);
+        console.log(bag);
+        return isVisible;
+    };
+    Treemap.prototype.areAllBelow = function (xs, max, debug) {
+        console.log("are all below?", max, "list", xs, debug);
+        if (xs.length == 0) {
+            return true;
+        }
+        return _.every(xs, function (i) { return i < max; });
+    };
+    Treemap.prototype.maxTreeHeight = function (xs) {
+        return _.max(xs);
+    };
+    return Treemap;
+}());
 function exec() {
     console.log("Day Eight!");
-    console.log(input());
+    var totalVisible = 0;
+    var mt = new Treemap(input());
+    for (var x = 0; x < mt.width; x++) {
+        for (var y = 0; y < mt.height; y++) {
+            if (mt.isVisible(x, y)) {
+                totalVisible++;
+            }
+        }
+        console.log("===");
+    }
+    // console.log(mt.isVisible(1, 1));
+    console.log("Total Visible", totalVisible);
 }
 exports.exec = exec;
--- 2022/08.ts
+++ 2022/08.ts
@@ -1,9 +1,10 @@
 const fs = require("fs");
+const _ = require("lodash");
 
 function input(): number[][] {
   var data: number[][] = [[]];
   const raw: string[] = fs
-    .readFileSync("2022/input-08-ex.txt", "utf-8")
+    .readFileSync("2022/input-08.txt", "utf-8")
     .split("\n");
   for (var i = 0; i < raw.length; i++) {
     const line = raw[i];
@@ -17,9 +18,101 @@ function input(): number[][] {
   return data;
 }
 
+class Treemap {
+  map: number[][] = [[]];
+  width: number = 0;
+  height: number = 0;
+  constructor(rawmap: number[][]) {
+    this.map = rawmap;
+    this.width = rawmap[0].length;
+    this.height = rawmap.length;
+  }
+  isVisible(x: number, y: number): boolean {
+    const treeHeight = this.map[x][y];
+    var buff: number[] = [];
+    var bag = {
+      left: false,
+      right: false,
+      top: false,
+      bottom: false,
+    };
+    // !! Left/Right compare
+    const row = this.map[x];
+    console.log("ROW:", row);
+    for (var i = 0; i <= row.length; i++) {
+      if (i < y) {
+        // left side
+        buff.push(this.map[x][i]);
+      }
+      if (i == y) {
+        // compare what is to left of us
+        bag.left = this.areAllBelow(buff, treeHeight, "left");
+        buff = [];
+      }
+      if (i > y && i != row.length) {
+        // build right side
+        buff.push(this.map[x][i]);
+      }
+      if (i == row.length) {
+        // compare what is right of us
+        bag.right = this.areAllBelow(buff, treeHeight, "right");
+      }
+    }
+    buff = [];
+    const column: number[] = [];
+    for (var i = 0; i < this.height; i++) {
+      column.push(this.map[i][y]);
+    }
+    console.log("COLUMN:", column);
+    for (var i = 0; i <= column.length; i++) {
+      if (i < x) {
+        // top
+        buff.push(column[i]);
+      }
+      if (i == x) {
+        // compare top
+        bag.top = this.areAllBelow(buff, treeHeight, "top");
+        buff = [];
+      }
+      if (i > x && i != column.length) {
+        buff.push(column[i]);
+      }
+      if (i == column.length) {
+        bag.bottom = this.areAllBelow(buff, treeHeight, "bottom");
+        buff = [];
+      }
+    }
+    const isVisible = bag.left || bag.right || bag.top || bag.bottom;
+    console.log("POS", x, y, "Value", treeHeight, "Visible?", isVisible);
+    console.log(bag);
+    return isVisible;
+  }
+  areAllBelow(xs: number[], max: number, debug: string): boolean {
+    console.log("are all below?", max, "list", xs, debug);
+    if (xs.length == 0) {
+      return true;
+    }
+    return _.every(xs, (i: number) => i < max);
+  }
+  maxTreeHeight(xs: number[]): number {
+    return _.max(xs);
+  }
+}
+
 function exec() {
   console.log("Day Eight!");
-  console.log(input());
+  var totalVisible = 0;
+  const mt = new Treemap(input());
+  for (var x = 0; x < mt.width; x++) {
+    for (var y = 0; y < mt.height; y++) {
+      if (mt.isVisible(x, y)) {
+        totalVisible++;
+      }
+    }
+    console.log("===");
+  }
+  // console.log(mt.isVisible(1, 1));
+  console.log("Total Visible", totalVisible);
 }
 
 export { exec };
